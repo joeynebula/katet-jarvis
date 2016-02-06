2335b0552bf69885b56f9a9a8ad578e1f0441174
joeynebula <joey.baker@waterfield.com>
2016-02-06 16:00:33 -0600

    fixing redit to go url and adding http port

--- lita_config.rb
+++ lita_config.rb
@@ -25,8 +25,9 @@ Lita.configure do |config|
   # config.adapter.password = "secret"
 
   ## Example: Set options for the Redis connection.
-  config.redis.host = ENV["REDISTOGO_URL"]
+  config.redis[:url] = ENV["REDISTOGO_URL"]
   # config.redis.port = 1234
+  config.http.port = ENV["PORT"]
 
   ## Example: Set configuration for any loaded handlers. See the handler's
   ## documentation for options.
