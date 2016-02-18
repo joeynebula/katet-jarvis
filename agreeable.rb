f7ceb99e678a612833be53b9c566546948694145
joeynebula <joey.baker@waterfield.com>
2016-02-18 10:40:41 -0600

    editing the agree regex to include more patters

--- agreeable.rb
+++ agreeable.rb
@@ -2,7 +2,7 @@ module Lita
   module Handlers
     class Agreeable < Handler
       # insert handler code here
-      route(/^agree|agreeable\s+/,:agree, command: false)
+      route(/^agree|agreeable|agreed\s+/,:agree, command: false)
 
       def agree(response)
         response.reply "http://gph.is/176YdyA"
